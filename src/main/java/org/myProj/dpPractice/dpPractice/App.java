118b04d1c34be8af127d53aa9af1355e601d7858
ChiaFu Kuo <fatfatfu@gmail.com>
2018-01-28 22:34:13 +0800

    1st commit to proj

---
+++ src/main/java/org/myProj/dpPractice/dpPractice/App.java
@@ -0,0 +1,13 @@
+package org.myProj.dpPractice.dpPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
